539fc3426ef323fbd2a33420c953ce3db20f7a98
Michelle Huh <michelle.huh@hotmail.com>
2018-06-10 20:37:18 -0700

    Menu bar color change upon scrolling

--- my-app/src/components/main/MainPage.js
+++ my-app/src/components/main/MainPage.js
@@ -3,17 +3,54 @@ import React from 'react'
 import PropTypes from "prop-types";
 import LoginActions from "../../actions/LoginActions";
 import LoginStore from "../../stores/LoginStore";
-import Worksheet from "../worksheet/Worksheet";
-import { Menu, Container, Dropdown } from 'semantic-ui-react'
+import WorksheetPage from "../worksheet/WorksheetPage";
+import { Menu, Container, Dropdown, Sticky } from 'semantic-ui-react'
 
 
-class MainPage extends React.Component {
+export default class MainPage extends React.Component {
+
+    constructor () {
+        super();
+
+        this.MainMenu = React.createRef();
+    }
 
     state = { activeItem: 'worksheet' };
 
     handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
     logout = () => LoginActions.logout();
+
+
+    getOffset = (element) => {
+        let bounding = element.getBoundingClientRect();
+        return {
+            top: element.offsetTop + document.body.scrollTop,
+            left: bounding.left + document.body.scrollLeft
+        };
+    };
+
+    handleScroll = () => {
+        let windowsScrollTop  = window.pageYOffset;
+        let mainMenu = this.MainMenu.current.parentElement;
+        let offset = this.getOffset(mainMenu);
+        if(windowsScrollTop >= offset.top){
+
+            mainMenu.classList.add("inverted");
+            mainMenu.classList.add("custom");
+        }else if (mainMenu.classList.contains("inverted")){
+            mainMenu.classList.remove("inverted");
+            mainMenu.classList.remove("custom");
+        }
+    };
+
+    componentDidMount = () => {
+        window.addEventListener('scroll', this.handleScroll);
+    };
+
+    componentWillUnmount = () => {
+        window.removeEventListener('scroll', this.handleScroll);
+    };
 
     render() {
         const { activeItem } = this.state;
@@ -22,6 +59,7 @@ class MainPage extends React.Component {
         return (
             <div>
                 <Menu pointing secondary  id='MainMenu'>
+                    <span ref={this.MainMenu}></span>
                     <Menu.Item
                         name='worksheet'
                         active={activeItem === 'worksheet'}
@@ -47,10 +85,10 @@ class MainPage extends React.Component {
                         </Dropdown>
                     </Menu.Menu>
                 </Menu>
-
                 <Container>
-                    {activeItem === 'worksheet' && <Worksheet/>}
+                    {activeItem === 'worksheet' && <WorksheetPage/>}
                 </Container>
+
             </div>
         )
     }
@@ -61,5 +99,3 @@ MainPage.propTypes = {
         push: PropTypes.func.isRequired
     }).isRequired,
 };
-
-export default MainPage;
