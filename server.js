4128b211af1517a5912a5adc687c79404f84d622
Michelle Huh <michelle.huh@hotmail.com>
2018-07-21 14:18:50 -0700

    package updated for deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require('./server/models').connect(config.dbUri);
 const app = express();
 // tell the app to look for static files in these directories
 app.use(express.static('./server/static/'));
-app.use(express.static('./client/dist/'));
+app.use(express.static('./client/src/'));
 // tell the app to parse HTTP body messages
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
