5a512dd6b6f7d2b0efe9cdc906d39e55a9c30edd
Michelle Huh <michelle.huh@hotmail.com>
2018-08-04 08:20:44 -0700

    server.js updated

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ require('./server/models').connect(config.dbUri);
 const app = express();
 
 // Serve any static files
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/build/'));
 // tell the app to parse HTTP body messages
@@ -42,14 +43,10 @@ app.use(cors());
 
 app.use(flash());
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 
 // start the server
 app.listen(port, () => console.log(`Listening on port ${port}`));
