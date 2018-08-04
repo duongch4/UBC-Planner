3f2f21d9cb59983998e5e42816be9f421b6dbf50
Angelimc <a.corpin@alumni.ubc.ca>
2018-08-03 22:35:34 -0700

    heroku changes

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const port = process.env.PORT || 5000;
 require('./server/models').connect(config.dbUri);
 
 const app = express();
-// tell the app to look for static files in these directories
+
+// Serve any static files
+ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/build/'));
 // tell the app to parse HTTP body messages
@@ -39,6 +41,10 @@ app.use('/email', emailRoutes);
 app.use(cors());
 
 app.use(flash());
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 
 // start the server
