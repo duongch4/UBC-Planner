d35224d05caecc335ea4d7548e466037ea785196
michellehuh <michelle.huh@hotmail.com>
2018-08-03 23:03:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -42,5 +42,14 @@ app.use(cors());
 
 app.use(flash());
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 // start the server
 app.listen(port, () => console.log(`Listening on port ${port}`));
