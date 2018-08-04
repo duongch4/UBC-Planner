a7514622f979173f5ef052a53cac76dbc8293865
michellehuh <michelle.huh@hotmail.com>
2018-08-03 22:57:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ require('./server/models').connect(config.dbUri);
 const app = express();
 
 // Serve any static files
- app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/build/'));
 // tell the app to parse HTTP body messages
@@ -42,11 +41,6 @@ app.use('/email', emailRoutes);
 app.use(cors());
 
 app.use(flash());
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
 
 // start the server
 app.listen(port, () => console.log(`Listening on port ${port}`));
